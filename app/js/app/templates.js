786c436aca535b2c2f801a3a317c8fab8b32f18e
Nikhil <nikhil.sonnad@gmail.com>
2013-08-16 14:24:00 +1000

    add timeline page, include event for template:loaded

--- app/js/app/templates.js
+++ app/js/app/templates.js
@@ -1,8 +1,9 @@
 define([
   'jquery',
   'handlebars',
-  'require'
-], function(jquery, handlebars, require) {
+  'require',
+  'events'
+], function(jquery, handlebars, require, events) {
 
   var templateDir = location.pathname.split('/')[1];
   if (templateDir === '') {
@@ -43,6 +44,7 @@ define([
   var insertContent = function() {
     require([getTextFile('content.html')], function(text) {
       content.append(text);
+      events.trigger('template:loaded');
     })
   };
 
