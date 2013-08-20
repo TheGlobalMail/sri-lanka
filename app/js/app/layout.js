9d23aae483537098e07c8b1d4b2436935ba6a576
Mark Finger <markfinger@gmail.com>
2013-08-20 11:17:10 +1000

    Content changes and hooking up the footer nav as a clone of the header.

--- app/js/app/layout.js
+++ app/js/app/layout.js
@@ -3,6 +3,17 @@ define([
   'events'
 ], function($, events) {
 
+  var articleNav;
+  var content;
+
+  var footerNav = function() {
+    var footerNav = articleNav.clone();
+    footerNav
+      .removeClass('fixed')
+      .addClass('footer');
+    content.append(footerNav);
+  };
+
   var scaleToViewport = function() {
 //    $('.article-header').height(window.innerHeight);
   };
@@ -11,6 +22,11 @@ define([
     scaleToViewport();
 
     events.trigger('layout:complete');
+
+    articleNav = $('.article-nav-container');
+    content = $('.main-article');
+
+    footerNav();
   };
 
   return {
