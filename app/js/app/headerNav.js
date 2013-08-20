2e4d5e50bb6aec2cdfd6241f5d064704fa846822
Mark Finger <markfinger@gmail.com>
2013-08-20 17:31:27 +1000

    Making the header nav a bit more responsive.

--- app/js/app/headerNav.js
+++ app/js/app/headerNav.js
@@ -4,10 +4,24 @@ define([
   'lodash'
 ], function($, scroll, _) {
 
+  var navbar;
   var articleTitleContainer;
   var articleNav;
   var main;
   var header;
+
+  var slideNavDown = function() {
+    articleNav
+      .addClass('visible')
+      .css('top', navbar.outerHeight());
+  };
+
+  var slideNavUp = function() {
+    var topOffset = -articleNav.outerHeight(true);
+    articleNav
+      .removeClass('visible')
+      .css('top', topOffset);
+  };
 
   var fadeInNavBarText = function() {
     articleTitleContainer
@@ -16,12 +30,16 @@ define([
 
   var setBindings = function() {
     articleTitleContainer.on('click', function() {
-      articleNav.toggleClass('visible');
+      if (articleNav.hasClass('visible')) {
+        slideNavUp();
+      } else {
+        slideNavDown();
+      }
       articleTitleContainer.toggleClass('child-nav-visible');
       main.one('click', function() {
-        articleNav.removeClass('visible');
+        slideNavUp();
         articleTitleContainer.removeClass('child-nav-visible');
-      })
+      });
     });
 
     scroll.observe(header, {
@@ -31,11 +49,13 @@ define([
   };
 
   var init = function() {
+    navbar = $('.navbar');
     articleTitleContainer = $('.article-title-container');
-    articleNav = $('.article-nav-container');
+    articleNav = $('.article-nav-container.fixed');
     main = $('#main');
     header = $('.header');
     setBindings();
+    slideNavUp();
   };
 
   return {
