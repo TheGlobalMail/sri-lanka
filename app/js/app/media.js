89aac68bacd2da21c68b8a3b17869bb6d2661a3d
Mark Finger <markfinger@gmail.com>
2013-08-21 10:01:59 +1000

    Hooking up the responsive images.

--- app/js/app/media.js
+++ app/js/app/media.js
@@ -4,24 +4,50 @@ define([
 ], function($, events) {
 
   var articleHeader;
+  var responsiveImages;
+
+  var mediaLoaded = function() {
+    events.trigger('media:loaded');
+  };
+
+  var onBGImageLoad = function(element, callback) {
+    var elementSrc = element
+      .css('background-image')
+      .replace('url(', '').slice(0, -1);
+    var image = $('<img>')
+      .attr('src', elementSrc);
+    image.on('load', function() {
+      callback(image[0]);
+    });
+  };
 
   var detectHeaderImageLoaded = function() {
     var image = articleHeader.find('.image');
-    var imageSrc = image.css('background-image').replace('url(', '').slice(0, -1);
-    $('<img>')
-      .attr('src', imageSrc)
-      .on('load', function() {
-        events.trigger('media:loaded');
+    onBGImageLoad(image, mediaLoaded);
+  };
+
+  var sizeResponsiveImages = function() {
+    // Expand each element to fill a 4:3 ratio
+    responsiveImages.each(function() {
+      var element = $(this);
+      onBGImageLoad(element, function(image) {
+        var imageRatio = image.width / image.height;
+        var height = element.width() / imageRatio;
+        element.css('height', height);
       });
+    })
   };
 
   var setBindings = function() {
     detectHeaderImageLoaded();
+    $(window).on('resize', _.debounce(sizeResponsiveImages, 50));
   };
 
   var init = function() {
     articleHeader = $('.article-header');
+    responsiveImages = $('.responsive-image-container');
 
+    sizeResponsiveImages();
     setBindings();
   };
 
